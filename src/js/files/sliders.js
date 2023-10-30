21a4da74fde6ed5ff8fd87bf783d7fcac0c08eaa
osdal <osdal67@gmail.com>
2023-10-30 14:07:56 +0200

    156156156

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -117,11 +117,9 @@ function initSliders() {
 			}
 		});
 		var swiper = new Swiper(".mySwiper", {
-			slidesPerView: 3,
-			spaceBetween: 30,
-			pagination: {
-				el: ".swiper-pagination",
-				clickable: true,
+			navigation: {
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev",
 			},
 		});
 	}
